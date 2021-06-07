71089454cfd642c1d54a342acc54faafa372474e
Daphney08 <egladyskristine@gmail.com>
2021-06-07 16:21:00 -0700

    ok

---
+++ routes/modules/asset.blade.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AssetController;
+
+
+Route::get('/asset', [ 
+    SupplierController::class, 'index' 
+    ])->name('asset');
+
+
+Route::get('/asset/create', [ 
+    SupplierController::class, 'create'
+     ])->name('asset.create');
+
+
+
+Route::post('/asset/create/save', 
+[ SupplierController::class, 'store' ])
+->name('asset.create.save');
+
+
+
+Route::get('/asset/edit/{id}', [
+     SupplierController::class, 'edit' 
+     ])->name('asset.edit');
+
+
+
+Route::post('/asset/update/{id}', [
+     SupplierController::class, 'update' 
+     ])->name('asset.update');
+
+
+Route::get('/asset/delete/{id}', [ 
+    SupplierController::class, 'delete' 
+    ])->name('asset.delete');
+
